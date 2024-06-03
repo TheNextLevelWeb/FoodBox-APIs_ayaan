8b4e237fc2afc8e861aeba5dfcf1d659fea18745
TheNextLevelWeb <khushali@nextlevelbranding.in>
2024-06-03 16:35:08 +0530

    Update urls.py

--- FoodBox APIs/FoodBox/urls.py
+++ FoodBox APIs/FoodBox/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-
+admin.site.site_header = "FoodBox Admin"
+admin.site.site_title = "FoodBox Admin Panel"
+admin.site.index_title = "Welcome to FoodBox Admin Panel"
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('users.urls')),
